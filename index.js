6cc253b303510584769664dad8b92826fe2488b5
nisha <nisha.4r@gmail.com>
2023-05-03 08:01:21 -0600

    all prompts

--- index.js
+++ index.js
@@ -9,10 +9,26 @@ function question() {
     inquirer.prompt([{
         type: "input",
         message: "Enter the text to display in your logo (max 3 characters)",
-        name: "text"
+        name: "logoname"
     },
     {
-        
+        type: "input",
+        message: "Enter text color either as color keyword or hexadecimal number",
+        name: "color"
+
+    },
+    {
+        type: "list",
+        message: "Choose the shape of your logo",
+        choices: ["Triangle", "Circle", "Square"],
+        name: "shape"
+
+    },
+    {
+        type: "input",
+        message: "Enter the shape's color of your choice, input value should be : color keyword or hexadecimal number",
+        name: "shapecolor"
+
     }
 ])
 }
