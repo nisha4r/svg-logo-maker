0ada66009bcd6fb182d35cff16d6610de44f1209
nisha <nisha.4r@gmail.com>
2023-05-03 07:50:44 -0600

    adding function names

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const { Triangle, Circle, Square } = require("./lib/shapes");
+
+function writeToFile(){
+    
+}
+function question(){
+
+}
+
+question();
