8daf3059354ee650849bcad0ee73e97db42bd1b1
nisha <nisha.4r@gmail.com>
2023-05-03 07:53:47 -0600

    enter text for logo

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const { Triangle, Circle, Square } = require("./lib/shapes");
 
-function writeToFile(){
-    
-}
-function question(){
+function writeToFile() {
 
+}
+function question() {
+    inquirer.prompt([{
+        type: "input",
+        message: "Enter the text to display in your logo (max 3 characters)",
+        name: "text"
+    },
+    {
+        
+    }
+])
 }
 
 question();
